720f73b0bb0745e6d8c8f6f80391a392825fcb54
Tobias-Ternent <tobias@ebi.ac.uk>
2018-05-30 12:05:56 +0100

    Updated formatting, cleaned up spectra deletion logic.

--- src/main/java/uk/ac/ebi/pride/spectrumindex/search/indexer/ProjectSpectraIndexer.java
+++ src/main/java/uk/ac/ebi/pride/spectrumindex/search/indexer/ProjectSpectraIndexer.java
@@ -2,8 +2,10 @@ package uk.ac.ebi.pride.spectrumindex.search.indexer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.PageRequest;
 import uk.ac.ebi.pride.spectrumindex.search.model.Spectrum;
 import uk.ac.ebi.pride.spectrumindex.search.service.SpectrumIndexService;
+import uk.ac.ebi.pride.spectrumindex.search.service.SpectrumSearchService;
 import uk.ac.ebi.pride.spectrumindex.search.util.SpectrumJmzReaderMapper;
 import uk.ac.ebi.pride.tools.jmzreader.JMzReaderException;
 import uk.ac.ebi.pride.tools.mgf_parser.MgfFile;
@@ -20,61 +22,76 @@ import java.util.List;
  */
 public class ProjectSpectraIndexer {
 
-    private static Logger logger = LoggerFactory.getLogger(ProjectSpectraIndexer.class.getName());
+  private static Logger logger = LoggerFactory.getLogger(ProjectSpectraIndexer.class.getName());
 
-    private int indexingSizeStep;
+  private int indexingSizeStep;
 
-    @Resource
-    private SpectrumIndexService spectrumIndexService;
+  @Resource private SpectrumIndexService spectrumIndexService;
+  @Resource private SpectrumSearchService spectrumSearchService;
 
+  public ProjectSpectraIndexer(
+      SpectrumIndexService spectrumIndexService,
+      SpectrumSearchService spectrumSearchService,
+      int indexingSizeStep) {
+    this.spectrumIndexService = spectrumIndexService;
+    this.spectrumSearchService = spectrumSearchService;
+    this.indexingSizeStep = indexingSizeStep;
+  }
 
-    public ProjectSpectraIndexer(SpectrumIndexService spectrumIndexService, int indexingSizeStep) {
-        this.spectrumIndexService = spectrumIndexService;
-        this.indexingSizeStep = indexingSizeStep;
-    }
-
-    public void indexAllSpectraForProjectAndAssay(String projectAccession, String assayAccession, File mgfFile){
-
-        long startTime;
-        long endTime;
-
-        startTime = System.currentTimeMillis();
-
-        try {
-            MgfFile inputParser = new MgfFile(mgfFile);
-            List<Spectrum> spectraToIndex = new LinkedList<Spectrum>();
-            logger.info("There are " + inputParser.getSpectraCount() + " spectra to index");
-            logger.info("Spectra index size step: " + indexingSizeStep);
-
-            for (int i=1; i<=inputParser.getSpectraCount();i++) {
-                Ms2Query spectrum = (Ms2Query) inputParser.getSpectrumByIndex(i);
-                Spectrum mongoSpectrum = SpectrumJmzReaderMapper.createMongoSpectrum(projectAccession, assayAccession, spectrum);
-                spectraToIndex.add(mongoSpectrum);
-                if (spectraToIndex.size() >= indexingSizeStep) {
-                    this.spectrumIndexService.save(spectraToIndex);
-                    spectraToIndex = new LinkedList<Spectrum>();
-                }
-            }
-            if (spectraToIndex.size()>0) { // Finally... the last bit
-                this.spectrumIndexService.save(spectraToIndex);
-            }
-
-        } catch (JMzReaderException e) {
-            e.printStackTrace();
+  public void indexAllSpectraForProjectAndAssay(
+      String projectAccession, String assayAccession, File mgfFile) {
+    long startTime;
+    long endTime;
+    startTime = System.currentTimeMillis();
+    try {
+      MgfFile inputParser = new MgfFile(mgfFile);
+      List<Spectrum> spectraToIndex = new LinkedList<Spectrum>();
+      logger.info("There are " + inputParser.getSpectraCount() + " spectra to index");
+      logger.info("Spectra index size step: " + indexingSizeStep);
+      for (int i = 1; i <= inputParser.getSpectraCount(); i++) {
+        Ms2Query spectrum = (Ms2Query) inputParser.getSpectrumByIndex(i);
+        Spectrum mongoSpectrum =
+            SpectrumJmzReaderMapper.createMongoSpectrum(projectAccession, assayAccession, spectrum);
+        spectraToIndex.add(mongoSpectrum);
+        if (spectraToIndex.size() >= indexingSizeStep) {
+          this.spectrumIndexService.save(spectraToIndex);
+          spectraToIndex = new LinkedList<Spectrum>();
         }
-
-
-        endTime = System.currentTimeMillis();
-        logger.info("DONE indexing all spectra for assay " + assayAccession + " in project " + projectAccession + " in " + (double) (endTime - startTime) / 1000.0 + " seconds");
-
+      }
+      if (spectraToIndex.size() > 0) { // Finally... the last bit
+        this.spectrumIndexService.save(spectraToIndex);
+      }
+    } catch (JMzReaderException e) {
+      e.printStackTrace();
     }
+    endTime = System.currentTimeMillis();
+    logger.info(
+        "DONE indexing all spectra for assay "
+            + assayAccession
+            + " in project "
+            + projectAccession
+            + " in "
+            + (double) (endTime - startTime) / 1000.0
+            + " seconds");
+  }
 
-
-    public void deleteAllSpectraForProject(String projectAccession) {
-        this.spectrumIndexService.deleteByProjectAccession(projectAccession);
+  public void deleteAllSpectraForProject(String projectAccession) {
+    int MAX_PAGE_SIZE = 1000;
+    long spectraCount = spectrumSearchService.countByProjectAccession(projectAccession);
+    List<Spectrum> initialSpectraFound;
+    while (0 < spectraCount) {
+      for (int i = 0; i < (spectraCount / MAX_PAGE_SIZE) + 1; i++) {
+        initialSpectraFound =
+            spectrumSearchService
+                .findByProjectAccession(projectAccession, new PageRequest(i, MAX_PAGE_SIZE))
+                .getContent();
+        spectrumIndexService.delete(initialSpectraFound);
+      }
+      spectraCount = spectrumSearchService.countByProjectAccession(projectAccession);
     }
+  }
 
-    public void setIndexingSizeStep(int indexingSizeStep) {
-        this.indexingSizeStep = indexingSizeStep;
-    }
+  public void setIndexingSizeStep(int indexingSizeStep) {
+    this.indexingSizeStep = indexingSizeStep;
+  }
 }
